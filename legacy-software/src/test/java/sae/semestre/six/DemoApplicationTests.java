4b78a5f4edb7f39a89c18c073552e47ae45a0443
Simon Guiraud <simon.guiraud12@gmail.com>
2025-05-15 08:36:15 +0200

    feat(Tests) : Fichier fonctionnel
    Fichier des tests d'intégration fonctionnel
    Modification pom.xml
    Ajout @Transactional

--- legacy-software/src/test/java/sae/semestre/six/DemoApplicationTests.java
+++ legacy-software/src/test/java/sae/semestre/six/DemoApplicationTests.java
@@ -1,11 +1,10 @@
 package sae.semestre.six;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
 
-@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
 @SpringBootTest
 public class DemoApplicationTests {
 
