af562286546095e6bddc659ed6c5f17cb1e10444
Simon Guiraud <simon.guiraud12@gmail.com>
2025-05-19 14:05:35 +0200

    fix(Tests) : Tests d'intégration
    Non fonctionnels

--- legacy-software/src/test/java/sae/semestre/six/DemoApplicationTests.java
+++ legacy-software/src/test/java/sae/semestre/six/DemoApplicationTests.java
@@ -1,6 +1,6 @@
 package sae.semestre.six;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
